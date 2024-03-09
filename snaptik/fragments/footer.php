06166a109b8d817581cce6499139d782a6b53b7a
Almafazi <almafazi@icloud.com>
2024-03-10 00:11:57 +0700

    initialize

---
+++ snaptik/fragments/footer.php
@@ -0,0 +1,37 @@
+<footer>
+    <div class="container">
+    <div class="row">
+    <div class="col-12">
+    <div class="z-menu">
+    <div>
+    <h3>Company</h3>
+    <ul>
+    <li><a href="/about-us">About Us</a></li>
+    <li><a href="/contact">Contact</a></li>
+    </ul>
+    </div>
+    <div>
+    <h3>Legal</h3>
+    <ul>
+    <li><a href="/tos">Terms of Service</a></li>
+    <li><a href="/privacy-policy">Privacy Policy</a></li>
+    </ul>
+    </div>
+    <div>
+    <h3>Tools</h3>
+    <ul>
+    <li><a href="/download-tiktok-slideshow/">Download TikTok Video</a></li>
+    <li><a href="/download-tiktok-story/">Download TikTok Story</a></li>
+    <li><a href="/download-tiktok-mp3/">Download TikTok MP3</a></li>
+    </ul>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    <div class="copyright">
+    <div class="container footer-copyright">
+    <div>&copy; 2024 (v1.1) <a href="/" title="SnapTik">SnapTik</a>. All rights reserved.</div>
+    </div>
+    </div>
+</footer>
---
+++ snaptik/fragments/langbar.php
@@ -0,0 +1,18 @@
+<ul class="dropdown-menu dropdown-lang">
+    <li class="lang-item" data-id="en"><a class="dropdown-item" href="/en/">English</a></li>
+    <li class="lang-item" data-id="ID"><a class="dropdown-item" href="/ID/">Bahasa Indonesia</a></li>
+    <li class="lang-item" data-id="vn"><a class="dropdown-item" href="/vn/">Ting Việt</a></li>
+    <li class="lang-item" data-id="ms"><a class="dropdown-item" href="/ms/">Bahasa Malaysia</a></li>
+    <li class="lang-item" data-id="jv"><a class="dropdown-item" href="/jv/">Basa Jawa</a></li>
+    <li class="lang-item" data-id="es"><a class="dropdown-item" href="/es/">Español</a></li>
+    <li class="lang-item" data-id="fr"><a class="dropdown-item" href="/fr/">Français</a></li>
+    <li class="lang-item" data-id="de"><a class="dropdown-item" href="/de/">German</a></li>
+    <li class="lang-item" data-id="it"><a class="dropdown-item" href="/it/">Italian</a></li>
+    <li class="lang-item" data-id="pt"><a class="dropdown-item" href="/pt/">Português</a></li>
+    <li class="lang-item" data-id="th"><a class="dropdown-item" href="/th/">Thailand</a></li>
+    <li class="lang-item" data-id="tr"><a class="dropdown-item" href="/tr/">Turkish (Turkey)</a></li>
+    <li class="lang-item" data-id="ar"><a class="dropdown-item" href="/ar/">عَرَبِّ</a></li>
+    <li class="lang-item" data-id="ko"><a class="dropdown-item" href="/ko/">한국어</a></li>
+    <li class="lang-item" data-id="ja"><a class="dropdown-item" href="/ja/">本語</a></li>
+    <li class="lang-item" data-id="in"><a class="dropdown-item" href="/in/">भारतीय भाषाएँ</a></li>
+</ul>
